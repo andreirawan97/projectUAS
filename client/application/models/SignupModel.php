bf7800d3442177675099e301050a443553bb3077
crevasse02 <wiboworaffi82@gmail.com>
2019-05-09 17:48:38 +0700

    fix conflict

--- client/application/models/SignupModel.php
+++ client/application/models/SignupModel.php
@@ -44,3 +44,23 @@
     }
 ?>
 
+<<<<<<< HEAD
+=======
+<?php
+      // $inputtedUserID = $_POST['inputtedUserID'];
+      // $inputtedFullName = $_POST['inputtedFullName'];
+      // $inputtedEmail = $_POST['inputtedEmail'];
+      // $inputtedPassword = $_POST['inputtedPassword'];
+      // $registerDate = $_POST['registerDate'];
+      // $response;
+      // if(!isDuplicateID($inputtedUserID) && !isDuplicateEmail($inputtedEmail)){
+      //   insertToDatabase($inputtedUserID, $inputtedFullName, $inputtedEmail, $inputtedPassword, null, $registerDate);
+        
+      //   $response = json_encode(array("status" => "ok", "message" => "Signup Succesful!"));
+      // }
+      // else{
+      //   $response = json_encode(array("status" => "err", "message" => "Email or ID has been used"));
+      // }
+      // echo $response;
+?>
+>>>>>>> Make signup and login design better
