1ff934b0577f563d4f86c9b8c51a61501532ed17
Andre Irawan <andreirawan97@gmail.com>
2019-05-16 19:20:50 +0700

    [Client][Home] Add carousel in home (#21)
    * Add carousel in home
    
    * Change carousel ID from example to promo
    
    * Change carousel text

--- client/application/views/pages/home.php
+++ client/application/views/pages/home.php
@@ -13,11 +13,48 @@
 </head>
 <body style="background-color: whitesmoke">
   <?php echo $NavigationBar ?>
-
-  <div class="container">
+  <div class="container" style="margin-bottom: 20px;">
     <div class="row">
       <div class="col-2"></div>
       <div class="col-8">
+        <!-- Carousel -->
+        <div class="bd-example" style="margin-top: 5px;">
+          <div id="carouselPromo" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselPromo" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselPromo" data-slide-to="1"></li>
+              <li data-target="#carouselPromo" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="<?php echo base_url('assets/promo1.jpg') ?>" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Comeback!</h5>
+                  <p>Now On SALE!!! Desert Sands Baby Roshan available to be purchased, Get it now</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="<?php echo base_url('assets/promo2.jpg') ?>" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="<?php echo base_url('assets/promo3.jpg') ?>" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselPromo" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselPromo" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+
         <!-- Search Bar -->
         <form id="searchForm">
           <div class="input-group mb-3" style="margin-top: 20px;">   
