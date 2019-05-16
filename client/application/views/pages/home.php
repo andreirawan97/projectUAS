8917b8e0da5e9e9e1bbf25fd5a7035486c20ffb6
Andre Irawan <andreirawan97@gmail.com>
2019-05-16 19:07:57 +0700

    Change carousel ID from example to promo

--- client/application/views/pages/home.php
+++ client/application/views/pages/home.php
@@ -19,11 +19,11 @@
       <div class="col-8">
         <!-- Carousel -->
         <div class="bd-example" style="margin-top: 5px;">
-          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <div id="carouselPromo" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+              <li data-target="#carouselPromo" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselPromo" data-slide-to="1"></li>
+              <li data-target="#carouselPromo" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
@@ -44,11 +44,11 @@
                 </div>
               </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselPromo" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carouselPromo" role="button" data-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
